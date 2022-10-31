1ac901f32ac32af2f1b739cb68796e0e2d1ab4e2
Parabeac-Nest <admin@parabeac.com>
2022-10-31 01:33:46 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/parabeac_play_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:parabeac_play/theme/parabeac_play_text_styles.g.dart';
+import 'package:parabeac_play/theme/parabeac_play_colors.g.dart';
+
+class ParabeacPlayTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: ParabeacPlayTextStyles.headline1,headline2: ParabeacPlayTextStyles.headline2,headline3: ParabeacPlayTextStyles.headline3,headline4: ParabeacPlayTextStyles.headline4,headline5: ParabeacPlayTextStyles.headline5,headline6: ParabeacPlayTextStyles.headline6,subtitle1: ParabeacPlayTextStyles.subtitle1,subtitle2: ParabeacPlayTextStyles.subtitle2,bodyText1: ParabeacPlayTextStyles.bodyText1,bodyText2: ParabeacPlayTextStyles.bodyText2,button: ParabeacPlayTextStyles.button,caption: ParabeacPlayTextStyles.caption,overline: ParabeacPlayTextStyles.overline,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: ParabeacPlayColors.lightOnPrimary,primary: ParabeacPlayColors.lightPrimary,primaryContainer: ParabeacPlayColors.lightPrimaryContainer,secondary: ParabeacPlayColors.lightSecondary,onSecondary: ParabeacPlayColors.lightOnSecondary,onPrimaryContainer: ParabeacPlayColors.lightOnPrimaryContainer,secondaryContainer: ParabeacPlayColors.lightSecondaryContainer,onSecondaryContainer: ParabeacPlayColors.lightOnSecondaryContainer,onTertiary: ParabeacPlayColors.lightOnTertiary,tertiary: ParabeacPlayColors.lightTertiary,tertiaryContainer: ParabeacPlayColors.lightTertiaryContainer,onTertiaryContainer: ParabeacPlayColors.lightOnTertiaryContainer,error: ParabeacPlayColors.lightError,onError: ParabeacPlayColors.lightOnError,errorContainer: ParabeacPlayColors.lightErrorContainer,onErrorContainer: ParabeacPlayColors.lightOnErrorContainer,background: ParabeacPlayColors.lightBackground,onBackground: ParabeacPlayColors.lightOnBackground,surface: ParabeacPlayColors.lightSurface,onSurface: ParabeacPlayColors.lightOnSurface,surfaceVariant: ParabeacPlayColors.lightSurfaceVariant,onSurfaceVariant: ParabeacPlayColors.lightOnSurfaceVariant,outline: ParabeacPlayColors.lightOutline,inverseSurface: ParabeacPlayColors.lightInverseSurface,onInverseSurface: ParabeacPlayColors.lightOnInverseSurface,inversePrimary: ParabeacPlayColors.lightInversePrimary,surfaceTint: ParabeacPlayColors.lightSurfaceTint);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: ParabeacPlayColors.darkPrimary,onPrimary: ParabeacPlayColors.darkOnPrimary,primaryContainer: ParabeacPlayColors.darkPrimaryContainer,onPrimaryContainer: ParabeacPlayColors.darkOnPrimaryContainer,secondary: ParabeacPlayColors.darkSecondary,onSecondary: ParabeacPlayColors.darkOnSecondary,secondaryContainer: ParabeacPlayColors.darkSecondaryContainer,onSecondaryContainer: ParabeacPlayColors.darkOnSecondaryContainer,tertiary: ParabeacPlayColors.darkTertiary,onTertiary: ParabeacPlayColors.darkOnTertiary,tertiaryContainer: ParabeacPlayColors.darkTertiaryContainer,onTertiaryContainer: ParabeacPlayColors.darkOnTertiaryContainer,error: ParabeacPlayColors.darkError,onError: ParabeacPlayColors.darkOnError,errorContainer: ParabeacPlayColors.darkErrorContainer,onErrorContainer: ParabeacPlayColors.darkOnErrorContainer,background: ParabeacPlayColors.darkBackground,onBackground: ParabeacPlayColors.darkOnBackground,surface: ParabeacPlayColors.darkSurface,onSurface: ParabeacPlayColors.darkOnSurface,surfaceVariant: ParabeacPlayColors.darkSurfaceVariant,outline: ParabeacPlayColors.darkOutline,inverseSurface: ParabeacPlayColors.darkInverseSurface,inversePrimary: ParabeacPlayColors.darkInversePrimary,surfaceTint: ParabeacPlayColors.darkSurfaceTint,onSurfaceVariant: ParabeacPlayColors.darkOnSurfaceVariant,onInverseSurface: ParabeacPlayColors.darkOnInverseSurface);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
